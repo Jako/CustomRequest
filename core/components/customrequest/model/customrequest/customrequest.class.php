b8ff45323e320cb5d8807355bc57f588959fe773
Thomas Jakobi <thomas.jakobi@partout.info>
2015-05-05 19:09:10 +0200

    Removed extended getOption method

--- core/components/customrequest/model/customrequest/customrequest.class.php
+++ core/components/customrequest/model/customrequest/customrequest.class.php
@@ -166,20 +166,20 @@ class CustomRequest
      *
      * @param string $key The option key to search for.
      * @param array $options An array of options that override local options.
-     * @param mixed $default The default value returned, if the option is not found locally or as a namespaced system setting.
-     * @param bool $skipEmpty If true: use default value if option value is empty.
+     * @param mixed $default The default value returned if the option is not found locally or as a
+     * namespaced system setting; by default this value is null.
      * @return mixed The option value or the default value specified.
      */
-    public function getOption($key, $options = array(), $default = null, $skipEmpty = false)
+    public function getOption($key, $options = array(), $default = null)
     {
         $option = $default;
         if (!empty($key) && is_string($key)) {
-            if ($options !== null && array_key_exists($key, $options) && !($skipEmpty && empty($options[$key]))) {
+            if ($options != null && array_key_exists($key, $options)) {
                 $option = $options[$key];
-            } elseif (array_key_exists($key, $this->options) && !($skipEmpty && empty($options[$key]))) {
+            } elseif (array_key_exists($key, $this->options)) {
                 $option = $this->options[$key];
             } elseif (array_key_exists("{$this->namespace}.{$key}", $this->modx->config)) {
-                $option = $this->modx->getOption("{$this->namespace}.{$key}", null, $default, $skipEmpty);
+                $option = $this->modx->getOption("{$this->namespace}.{$key}");
             }
         }
         return $option;
