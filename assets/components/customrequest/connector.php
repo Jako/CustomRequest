019aeeedbe1a8bac8a0bf3741ed89f8b78c3c167
Thomas Jakobi <thomas.jakobi@partout.info>
2016-09-14 14:52:06 +0200

    Updated package.json and refactored connector code

--- assets/components/customrequest/connector.php
+++ assets/components/customrequest/connector.php
@@ -3,24 +3,19 @@
  * CustomRequest Connector
  *
  * @package customrequest
+ * @subpackage connector
  */
 require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
 require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
 require_once MODX_CONNECTORS_PATH . 'index.php';
 
 $corePath = $modx->getOption('customrequest.core_path', null, $modx->getOption('core_path') . 'components/customrequest/');
+$customrequest = $modx->getService('customrequest', 'CustomRequest', $corePath . 'model/customrequest/', array(
+    'core_path' => $corePath
+));
 
-$modx->getService(
-    'customrequest',
-    'CustomRequest',
-    $corePath . 'model/customrequest/',
-    array(
-        'core_path' => $corePath
-    )
-);
-
-/* handle request */
+// Handle request
 $modx->request->handleRequest(array(
-    'processors_path' => $modx->customrequest->getOption('processorsPath'),
-    'location' => '',
+    'processors_path' => $customrequest->getOption('processorsPath'),
+    'location' => ''
 ));
