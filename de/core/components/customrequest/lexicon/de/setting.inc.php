012c08fce4329020be14983606249e62ca695faa
Thomas Jakobi <thomas.jakobi@partout.info>
2019-02-23 15:41:58 +0100

    Fixing wrong crowdin.yml

--- de/core/components/customrequest/lexicon/de/setting.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Setting Lexicon Entries for CustomRequest
- *
- * @package customrequest
- * @subpackage lexicon
- */
-$_lang['setting_customrequest.debug'] = 'Debug';
-$_lang['setting_customrequest.debug_desc'] = 'Debug-Informationen im MODX Fehlerprotokoll ausgeben.';
