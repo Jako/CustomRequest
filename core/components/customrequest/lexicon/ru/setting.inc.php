4b07aba7e0021824ecdd5528fa642e778b38fadf
Thomas Jakobi <thomas.jakobi@partout.info>
2019-04-03 08:14:58 +0200

    New translations setting.inc.php (Russian)

--- core/components/customrequest/lexicon/ru/setting.inc.php
+++ core/components/customrequest/lexicon/ru/setting.inc.php
@@ -5,5 +5,5 @@
  * @package customrequest
  * @subpackage lexicon
  */
-$_lang['setting_customrequest.debug'] = 'Debug';
-$_lang['setting_customrequest.debug_desc'] = 'Log debug informations in MODX error log.';
+$_lang['setting_customrequest.debug'] = 'Отладка';
+$_lang['setting_customrequest.debug_desc'] = 'Записывать отладочную информацию в лог ошибок MODX.';
