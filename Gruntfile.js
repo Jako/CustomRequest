dca90c2046b62c20234c6ad0986f7350ff77b5e1
Thomas Jakobi <thomas.jakobi@partout.info>
2019-07-18 18:18:44 +0200

    Fix autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,9 +60,7 @@ module.exports = function (grunt) {
             options: {
                 processors: [
                     require('pixrem')(),
-                    require('autoprefixer')({
-                        browsers: 'last 2 versions, ie >= 8'
-                    })
+                    require('autoprefixer')()
                 ]
             },
             mgr: {
