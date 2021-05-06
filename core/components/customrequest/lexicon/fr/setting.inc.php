94dc4864a9b18916e6a7f2ed6f1d7384d81f89e2
Thomas Jakobi <buero@treehillstudio.de>
2021-05-06 11:52:03 +0200

    Deleted translation using Weblate (French)

--- core/components/customrequest/lexicon/fr/setting.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Setting Lexicon Entries for CustomRequest
- *
- * @package customrequest
- * @subpackage lexicon
- */
-$_lang['setting_customrequest.debug'] = 'Debug';
-$_lang['setting_customrequest.debug_desc'] = 'Log debug informations in MODX error log.';
