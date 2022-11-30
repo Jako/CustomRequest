2f79a11a4dea88c4bfbef862c22a0f486af7747d
Thomas Jakobi <thomas.jakobi@partout.info>
2022-11-30 12:20:44 +0100

    Fix wrong class_exist check

--- core/components/customrequest/processors/mgr/settings/getlist.class.php
+++ core/components/customrequest/processors/mgr/settings/getlist.class.php
@@ -9,7 +9,7 @@
 // Compatibility between 2.x/3.x
 if (file_exists(MODX_PROCESSORS_PATH . 'system/settings/getlist.class.php')) {
     require_once MODX_PROCESSORS_PATH . 'system/settings/getlist.class.php';
-} elseif (!class_exists('modSystemSettingsUpdateProcessor')) {
+} elseif (!class_exists('modSystemSettingsGetListProcessor')) {
     class_alias(\MODX\Revolution\Processors\System\Settings\GetList::class, \modSystemSettingsGetListProcessor::class);
 }
 
