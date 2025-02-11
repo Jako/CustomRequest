186a25c97799b904f40175920061a95736576816
Thomas Jakobi <thomas.jakobi@partout.info>
2025-02-11 18:38:22 +0100

    Allow searching for the record id

--- core/components/customrequest/processors/mgr/settings/getlist.class.php
+++ core/components/customrequest/processors/mgr/settings/getlist.class.php
@@ -13,11 +13,23 @@ if (file_exists(MODX_PROCESSORS_PATH . 'system/settings/getlist.class.php')) {
     class_alias(\MODX\Revolution\Processors\System\Settings\GetList::class, \modSystemSettingsGetListProcessor::class);
 }
 
+/**
+ * Class CustomRequestSystemSettingsGetlistProcessor
+ */
 class CustomRequestSystemSettingsGetlistProcessor extends modSystemSettingsGetListProcessor
 {
     public $languageTopics = ['setting', 'namespace', 'customrequest:setting'];
     public $objectType = 'setting';
     public $primaryKeyField = 'key';
+
+    /**
+     * {@inheritDoc}
+     * @return bool
+     */
+    public function checkPermissions()
+    {
+        return !empty($this->permission) ? $this->modx->hasPermission($this->permission) || $this->modx->hasPermission('customrequest_' . $this->permission) : true;
+    }
 
     /**
      * {@inheritDoc}
