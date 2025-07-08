0b5654b80d065057b2779584c7472b1e6b1ea922
Thomas Jakobi <thomas.jakobi@partout.info>
2025-07-08 12:57:34 +0200

    Make the namespace no longer configurable!

--- core/components/customrequest/src/CustomRequest.php
+++ core/components/customrequest/src/CustomRequest.php
@@ -71,7 +71,6 @@ class CustomRequest
     public function __construct(modX &$modx, $options = [])
     {
         $this->modx =& $modx;
-        $this->namespace = $this->getOption('namespace', $options, $this->namespace);
 
         $corePath = $this->getOption('core_path', $options, $this->modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/' . $this->namespace . '/');
         $assetsPath = $this->getOption('assets_path', $options, $this->modx->getOption('assets_path', null, MODX_ASSETS_PATH) . 'components/' . $this->namespace . '/');
