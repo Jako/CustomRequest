ef6e7544bf2cc6db677dff43846df7e836250ce9
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 13:03:31 +0100

    Deleted translation using Weblate (Spanish)

--- core/components/customrequest/lexicon/es/setting.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Setting Lexicon Entries for CustomRequest
- *
- * @package customrequest
- * @subpackage lexicon
- */
-$_lang['setting_customrequest.debug'] = '';
-$_lang['setting_customrequest.debug_desc'] = '';
