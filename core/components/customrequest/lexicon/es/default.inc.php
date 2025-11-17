68b1dba67ca1fbe543dfd8fb8cb7ac4099cbebdd
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:43:04 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/customrequest/lexicon/es/default.inc.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Default Lexicon Entries for CustomRequest
+ *
+ * @package customrequest
+ * @subpackage lexicon
+ */
+$_lang['customrequest'] = '';
+$_lang['customrequest.configs'] = '';
+$_lang['customrequest.configs_alias'] = '';
+$_lang['customrequest.configs_alias_desc'] = '';
+$_lang['customrequest.configs_alias_generated'] = '';
+$_lang['customrequest.configs_alias_regex'] = '';
+$_lang['customrequest.configs_context'] = '';
+$_lang['customrequest.configs_create'] = '';
+$_lang['customrequest.configs_desc'] = '';
+$_lang['customrequest.configs_desc_extended'] = '';
+$_lang['customrequest.configs_duplicate'] = '';
+$_lang['customrequest.configs_err_ns_alias_resourceid'] = '';
+$_lang['customrequest.configs_err_nv_alias_regex'] = '';
+$_lang['customrequest.configs_err_nv_regex'] = '';
+$_lang['customrequest.configs_name'] = '';
+$_lang['customrequest.configs_regex'] = '';
+$_lang['customrequest.configs_regex_desc'] = '';
+$_lang['customrequest.configs_remove'] = '';
+$_lang['customrequest.configs_remove_confirm'] = '';
+$_lang['customrequest.configs_resourceid'] = '';
+$_lang['customrequest.configs_resourceid_desc'] = '';
+$_lang['customrequest.configs_update'] = '';
+$_lang['customrequest.configs_urlparams'] = '';
+$_lang['customrequest.configs_urlparams_desc'] = '';
+$_lang['customrequest.debug_mode'] = '';
+$_lang['customrequest.duplicate'] = '';
+$_lang['customrequest.menu_home'] = '';
+$_lang['customrequest.menu_home_desc'] = '';
+$_lang['customrequest.refresh_cache'] = '';
+$_lang['customrequest.settings'] = '';
+$_lang['customrequest.settings_desc'] = '';
+$_lang['customrequest.systemsetting_key_err_nv'] = '';
+$_lang['customrequest.systemsetting_usergroup_err_nv'] = '';
