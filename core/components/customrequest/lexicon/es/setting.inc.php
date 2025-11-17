17c843b60569edbd165389d5b9b9be5c76b46e41
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:43:16 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/customrequest/lexicon/es/setting.inc.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Setting Lexicon Entries for CustomRequest
+ *
+ * @package customrequest
+ * @subpackage lexicon
+ */
+$_lang['setting_customrequest.debug'] = '';
+$_lang['setting_customrequest.debug_desc'] = '';
